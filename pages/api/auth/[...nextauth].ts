58f2b7e29f9b278c82ca19ea2ab53a3717a28043
idchef <tymoteusz.kossek@gmail.com>
2023-01-07 13:04:23 +0100

    feat: save auth data in db, add Discord provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,20 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import DiscordProvider from "next-auth/providers/discord";
+import prisma from "prisma/prismaclient";
+
 export const authOptions = {
-    // Configure one or more authentication providers
+    adapter: PrismaAdapter(prisma),
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID || "",
             clientSecret: process.env.GITHUB_SECRET || "",
         }),
-        // ...add more providers here
+        DiscordProvider({
+            clientId: process.env.DISCORD_CLIENT_ID || "",
+            clientSecret: process.env.DISCORD_CLIENT_SECRET || "",
+        }),
     ],
 };
 export default NextAuth(authOptions);
