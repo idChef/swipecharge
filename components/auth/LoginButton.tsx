f40cc454f4ee3995a56257b57206d7afabcb94de
idchef <tymoteusz.kossek@gmail.com>
2023-01-07 12:17:12 +0100

    feat: install NextAuth, add Github provider

---
+++ components/auth/LoginButton.tsx
@@ -0,0 +1,29 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function LoginButton() {
+    const { data: session } = useSession();
+    if (session && session.user) {
+        return (
+            <>
+                Signed in as {session.user.email} <br />
+                <button
+                    className="border rounded px-4 py-2"
+                    onClick={() => signOut()}
+                >
+                    Sign out
+                </button>
+            </>
+        );
+    }
+    return (
+        <>
+            Not signed in <br />
+            <button
+                className="border rounded px-4 py-2"
+                onClick={() => signIn()}
+            >
+                Sign in
+            </button>
+        </>
+    );
+}
