13e6780fa55172627d4f406c7e23c327e5868d8e
idchef <tymoteusz.kossek@gmail.com>
2023-01-07 13:05:34 +0100

    fix: remove Github provider
    Github provider has issues with NextAuth Prisma adapter, hence the choice to remove it

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,16 +1,11 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
 import DiscordProvider from "next-auth/providers/discord";
 import prisma from "prisma/prismaclient";
 
 export const authOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID || "",
-            clientSecret: process.env.GITHUB_SECRET || "",
-        }),
         DiscordProvider({
             clientId: process.env.DISCORD_CLIENT_ID || "",
             clientSecret: process.env.DISCORD_CLIENT_SECRET || "",
