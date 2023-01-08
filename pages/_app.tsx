b10c5166bc0b638bc774f9814ab28d35f8cfab26
idchef <tymoteusz.kossek@gmail.com>
2023-01-08 12:58:05 +0100

    feat: install useSWR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,23 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { SWRConfig } from "swr";
 
 export default function App({
     Component,
     pageProps: { session, ...pageProps },
 }: AppProps) {
     return (
-        <SessionProvider session={session}>
-            <Component {...pageProps} />
-        </SessionProvider>
+        <SWRConfig
+            value={{
+                refreshInterval: 3000,
+                fetcher: (resource, init) =>
+                    fetch(resource, init).then((res) => res.json()),
+            }}
+        >
+            <SessionProvider session={session}>
+                <Component {...pageProps} />
+            </SessionProvider>
+        </SWRConfig>
     );
 }
