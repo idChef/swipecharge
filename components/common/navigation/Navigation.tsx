91feeb6476b29f5c84739d60389f1c8706722f03
idchef <tymoteusz.kossek@gmail.com>
2023-03-03 16:39:27 +0100

    feat: update navigation styles

--- components/common/navigation/Navigation.tsx
+++ components/common/navigation/Navigation.tsx
@@ -1,26 +1,57 @@
 import { FunctionComponent } from "react";
-import { HomeIcon, UserGroupIcon } from "@heroicons/react/24/solid";
+import {
+    HomeIcon,
+    UserGroupIcon,
+    BanknotesIcon,
+    UserIcon,
+    Cog6ToothIcon,
+} from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const ROUTES = [
     {
         name: "Home",
         path: "/",
-        icon: <HomeIcon />,
+        icon: <HomeIcon className="stroke-2" />,
     },
     {
         name: "Groups",
         path: "/groups",
-        icon: <UserGroupIcon />,
+        icon: <UserGroupIcon className="stroke-2" />,
+    },
+    {
+        name: "Expense",
+        path: "/expense/add",
+        icon: <BanknotesIcon className="stroke-2" />,
+    },
+    {
+        name: "Profile",
+        path: "/profile/me",
+        icon: <UserIcon className="stroke-2" />,
+    },
+    {
+        name: "Settings",
+        path: "/settings",
+        icon: <Cog6ToothIcon className="stroke-2" />,
     },
 ];
 
 export const Navigation: FunctionComponent = ({}) => {
+    const router = useRouter();
+
     return (
-        <div className="flex items-center gap-4 fixed bottom-0 drop-shadow-sm border-t border-black w-full left-0 h-12">
+        <div className="fixed bottom-0 left-0 flex h-24 w-full items-center gap-6 border-t px-4  drop-shadow-sm">
             {ROUTES.map(({ path, icon, name }) => (
-                <Link key={name} href={path}>
-                    <div className="w-8">{icon}</div>
+                <Link
+                    key={name}
+                    href={path}
+                    className={`flex flex-col items-center justify-center text-gray-400 ${
+                        router.pathname === path || router.pathname.startsWith(`${path}/`) ? "text-black" : ""
+                    }`}
+                >
+                    <div className="w-6">{icon}</div>
+                    <span className="text-center">{name}</span>
                 </Link>
             ))}
         </div>
