8f90687c9116804b34d100ad689c2e3edc7fca3c
idchef <tymoteusz.kossek@gmail.com>
2023-03-05 11:31:24 +0100

    feat: add sign out functionality

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import { FunctionComponent, ReactNode } from "react";
 import { Navigation } from "./common/navigation/Navigation";
 
@@ -5,7 +6,19 @@ interface LayoutProps {
     children: ReactNode;
 }
 
+const EXCLUDED_ROUTES = ["/login", "/signup"];
+
 const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
+    const router = useRouter();
+
+    if (EXCLUDED_ROUTES.includes(router.pathname)) {
+        return (
+            <div className="h-full bg-neutral-50 px-4 py-4 dark:bg-neutral-900">
+                {children}
+            </div>
+        );
+    }
+
     return (
         <div className="h-full bg-neutral-50 px-4 py-4 pb-24 dark:bg-neutral-900">
             <Navigation />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
-import LoginButton from "../components/auth/LoginButton";
-import useSWR from "swr";
 import { getSession, useSession } from "next-auth/react";
-import type { Group } from "@prisma/client";
-import { Header } from "components/common/header/Header";
 import { GetServerSideProps } from "next";
 import { NextRouter } from "next/router";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,8 +24,23 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Header heading="Dashboard" showBack={false} />
-            <LoginButton />
+            <div className="flex items-center justify-between">
+                <div className="flex flex-col">
+                    <span className="text-black dark:text-white">
+                        Hi {session?.user?.name} 👋
+                    </span>
+                    <h1 className="text-3xl text-black dark:text-white">
+                        Manage your finances
+                    </h1>
+                </div>
+                <div className="relative h-10 w-10 overflow-hidden rounded-full">
+                    <Image
+                        src={session?.user.image || ""}
+                        alt={session?.user.name || ""}
+                        fill
+                    />
+                </div>
+            </div>
         </>
     );
 }
