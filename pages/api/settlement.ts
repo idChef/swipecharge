761532e1bbc32d20ef23130f97e853f6c3668bf3
idchef <tymoteusz.kossek@gmail.com>
2023-03-15 17:26:27 +0100

    feat: settlements schema and endpoint

---
+++ pages/api/settlement.ts
@@ -0,0 +1,68 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "prisma/prismaclient";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const { method } = req;
+
+    switch (method) {
+        case "POST":
+            const { payerId, payeeId, amount, groupId } = req.body;
+
+            try {
+                const newSettlement = await client.settlement.create({
+                    data: {
+                        payerId,
+                        payeeId,
+                        amount,
+                        groupId,
+                    },
+                });
+
+                res.status(201).json(newSettlement);
+            } catch (error) {
+                res.status(500).json({ message: error });
+            }
+            break;
+
+        case "GET":
+            try {
+                const settlements = await client.settlement.findMany();
+                res.status(200).json(settlements);
+            } catch (error) {
+                res.status(500).json({ message: error });
+            }
+            break;
+
+        case "PUT":
+            const { id, updateData } = req.body;
+
+            try {
+                const updatedSettlement = await client.settlement.update({
+                    where: { id },
+                    data: updateData,
+                });
+
+                res.status(200).json(updatedSettlement);
+            } catch (error) {
+                res.status(500).json({ message: error });
+            }
+            break;
+
+        case "DELETE":
+            const { settlementId } = req.body;
+
+            try {
+                await client.settlement.delete({ where: { id: settlementId } });
+                res.status(204).end();
+            } catch (error) {
+                res.status(500).json({ message: error });
+            }
+            break;
+
+        default:
+            res.setHeader("Allow", ["POST", "GET", "PUT", "DELETE"]);
+            res.status(405).end(`Method ${method} Not Allowed`);
+    }
+};
+
+export default handler;
