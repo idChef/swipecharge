2040701572f2524832ea5e286928f70465146a50
idchef <tymoteusz.kossek@gmail.com>
2023-03-16 23:36:37 +0100

    feat: added current budgets

--- components/groups/BudgetTab.tsx
+++ components/groups/BudgetTab.tsx
@@ -16,27 +16,65 @@ type BudgetWithRestrictionsAndUser = Budget & {
 export const BudgetTab: FunctionComponent<BudgetTabProps> = ({}) => {
     const router = useRouter();
     const { groupId } = router.query;
-    const { data: budget, isLoading } = useSWR<BudgetWithRestrictionsAndUser[]>(
-        `/api/group/${groupId}/budget`
-    );
+    const { data: historyBudgets, isLoading } = useSWR<
+        BudgetWithRestrictionsAndUser[]
+    >(`/api/group/${groupId}/budget`);
+    const { data: currentBudget, isLoading: isLoadingCurrentBudget } =
+        useSWR<BudgetWithRestrictionsAndUser>(
+            `/api/group/${groupId}/budget?currentMonth=true`
+        );
 
-    if (isLoading) {
+    if (isLoading || isLoadingCurrentBudget) {
         return <div>Loading...</div>;
     }
 
     return (
         <div>
-            {(!budget || budget.length < 1) && (
+            {(!historyBudgets || historyBudgets.length < 1) && (
                 <div className="mt-2 rounded bg-black p-4 text-neutral-300">
                     No budgets yet
                 </div>
             )}
-            <h3 className="text-xl text-white">Current budget</h3>
 
-            {budget && budget.length > 0 && (
+            {currentBudget && (
+                <div className="mt-2 flex flex-col gap-3">
+                    <h3 className="text-xl text-white">Current budget</h3>
+                    <div
+                        key={currentBudget.id}
+                        className="flex justify-between gap-2 rounded bg-black p-4 text-neutral-300"
+                    >
+                        <div>
+                            {currentBudget.restrictions.map((restriction) => (
+                                <div
+                                    key={restriction.id}
+                                    className="flex items-center gap-2"
+                                >
+                                    <div>{restriction.categoryId}</div>
+                                    <div>
+                                        {amountFormatter(restriction.amount)}{" "}
+                                        PLN
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                        <div>
+                            {formatDateTime(currentBudget.yearAndMonth)}
+                            <div className="mt-2 flex items-center gap-2 text-sm">
+                                <img
+                                    className="h-8 w-8 rounded-full"
+                                    src={currentBudget.user.image}
+                                />
+                                Created by {currentBudget.user.name}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {historyBudgets && historyBudgets.length > 0 && (
                 <div className="mt-2 flex flex-col gap-3">
                     <h3 className="text-xl text-white">Budget history</h3>
-                    {budget?.map((budget) => (
+                    {historyBudgets?.map((budget) => (
                         <div
                             key={budget.id}
                             className="flex justify-between gap-2 rounded bg-black p-4 text-neutral-300"
--- pages/api/group/[groupId]/budget/current.ts
+++ pages/api/group/[groupId]/budget/current.ts
@@ -44,43 +44,12 @@ export default async function handler(
                 },
             });
 
-            res.status(200).json(currentMonth ? budgets[0] : budgets);
+            res.status(200).json(budgets);
 
             break;
-        case "POST":
-            const createdBudget = await client.budget.create({
-                data: {
-                    ...req.body,
-                    yearAndMonth: new Date(req.body.yearAndMonth),
-                    restrictions: {
-                        create: req.body.restrictions,
-                    },
-                },
-            });
-            res.status(201).json(createdBudget);
-            break;
-        case "PUT":
-            const id = req.body.id;
-            const updatedBudget = await client.budget.update({
-                where: { id },
-                data: {
-                    ...req.body,
-                    yearAndMonth: new Date(req.body.yearAndMonth),
-                    restrictions: {
-                        set: req.body.restrictions,
-                    },
-                },
-            });
-            res.status(200).json(updatedBudget);
-            break;
-        case "DELETE":
-            const deletedBudget = await client.budget.delete({
-                where: { id: req.body.id },
-            });
-            res.status(200).json(deletedBudget);
-            break;
+
         default:
-            res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+            res.setHeader("Allow", ["GET"]);
             res.status(405).end(`Method ${method} Not Allowed`);
     }
 }
